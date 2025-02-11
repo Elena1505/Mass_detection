e3aed13e7803ffa89b6d24f0cbcd1318569b2655
Elena1505 <eb.lelou@gmail.com>
2025-02-11 14:04:14 +0100

    Added some data explorations

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from src.data_loader import load_data
+from src.data_loader import load_data, explore_data
 
 def main():
     csv_path = "/home/lelou1505/DS/mass_detection/data/train-small.csv"
-    df = load_data(csv_path=csv_path)
+    df = load_data(file_path=csv_path)
+    explore_data(df)
 
 
 if __name__ == "__main__":
--- src/data_loader.py
+++ src/data_loader.py
@@ -5,8 +5,14 @@ import seaborn as sns
 from pandas import DataFrame
 
 
-def load_data(csv_path:str) -> DataFrame:
-    train_df = pd.read_csv(csv_path)
+def load_data(file_path:str) -> DataFrame:
+    train_df = pd.read_csv(file_path)
     print(f'Le dataset contient: {train_df.shape[0]} lignes et {train_df.shape[1]} colonnes.')
     return train_df
 
+
+def explore_data(df:DataFrame): 
+    print(df.info())
+    print(df.head())
+
+    
